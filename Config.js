205b8f13204f6296b7c4ef6700b60ce3f42f38b8
DistilleryHub <thedistillerymaster@gmail.com>
2026-09-06 19:05:12 +0530

    config.js

--- Config.js
+++ Config.js
@@ -1,5 +1,9 @@
 window.APP_CONFIG = {
-    features: { feed: true, chat: true, calling: true, jobs: true, articles: true, market: true, videos: true, files: true, learning: true, admin: false },
-    turn: { enabled: false, endpoint: "/getTurnCredentials" },
-    cloudinary: { cloudName: "y8iguof1", uploadPreset: "tdm_upload" }
+    features: {
+        feed: true, chat: true, calling: true, jobs: true,
+        articles: true, market: true, videos: true, files: true,
+        learning: true, admin: false
+    },
+    turn: { enabled: false },
+    cloudinary: { cloudName: "y8iguofl", uploadPreset: "tdm_upload" }
 };
