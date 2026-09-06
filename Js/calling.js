aa4e9a4c79a00b7d718e2746274ab9d5cbe26f97
DistilleryHub <thedistillerymaster@gmail.com>
2026-09-06 22:41:39 +0530

    calling.js

--- Js/calling.js
+++ Js/calling.js
@@ -3,7 +3,7 @@ import { auth } from './firebase-init.js';
 export async function startCall(otherUid, type) {
     if (!auth.currentUser) { alert('Please login first'); return; }
     if (!navigator.mediaDevices) { alert('Calling not supported'); return; }
-
+    
     try {
         const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: type === 'video' });
         alert('Call started (Mic/Camera accessed)');
