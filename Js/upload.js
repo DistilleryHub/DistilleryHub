66ce5a95c8f4fe8bd4749848a09da10b965dbb1b
DistilleryHub <thedistillerymaster@gmail.com>
2026-09-06 21:51:29 +0530

    upload.js

--- Js/upload.js
+++ Js/upload.js
@@ -1,7 +1,6 @@
 import { storage, auth } from './firebase-init.js';
 import { ref as sRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-storage.js";
 
-// फोटो/वीडियो अपलोड करें
 export async function uploadFile(file) {
     const uid = auth.currentUser.uid;
     const path = `uploads/${uid}/${Date.now()}_${file.name}`;
