5177fe2c23e320be21a6b4b3449c3db946273cdb
DistilleryHub <thedistillerymaster@gmail.com>
2026-09-07 21:23:06 +0530

    config.js

---
+++ js/Config.js
@@ -0,0 +1,17 @@
+// js/config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyB63lPTtic1RUjfq-KXWrvtisSGIetXL6k",
+  authDomain: "distilleryhub-b1d2d.firebaseapp.com",
+  projectId: "distilleryhub-b1d2d",
+  storageBucket: "distilleryhub-b1d2d.firebasestorage.app",
+  messagingSenderId: "221084904588",
+  appId: "1:221084904588:web:f1c47a722b2a7c98509fa9",
+  measurementId: "G-66L58LCCVY"
+};
+
+export const CLOUDINARY_CONFIG = {
+  cloudName: "y8iguof1",
+  uploadPreset: "tdm_upload"
+};
+
+export const VAPID_KEY = "YOUR_VAPID_KEY_FROM_FIREBASE"; // यहाँ अपनी असली Key डालें
